da2b9b4a7f3d836a6038648871315e19b6c42018
Abhinav-Achuta <aachuta@gmail.com>
2023-09-12 20:10:37 -0500

    Deleted test files

--- In class assignments/test.py
+++
@@ -1,7 +0,0 @@
-set = [[0]*5 for num in range(5)]
-
-print(set)
-
-print()
-
-print(set)
--- test.py
+++
@@ -1,9 +0,0 @@
-student_info = {
-        'Reagan' : 'rebradshaw835',
-        'Ryley' : 'rbarber894',
-        'Peyton' : 'pstott885',
-        'Tyrese' : 'tmayo945',
-        'Caius' : 'ccharlton329'
-    }
-
-print(student_info['Reagan'])
